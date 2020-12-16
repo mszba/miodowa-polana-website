0abec14c9bf719073e4f3b368a77175f3f6c792b
Marek Szuba <mszuba01@gmail.com>
2020-12-16 22:13:46 +0100

    add animationPresence from framer-motion

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 
 import Home from './pages/index';
 import AttractionsPage from './pages/attractions';
 import OfferPage from './pages/offer';
 import GalleryPage from './pages/gallery';
 
+import { AnimatePresence } from 'framer-motion';
+import styled from 'styled-components';
+
 import './App.css';
 
+const Section = styled.section`
+  /* overflow-x: hidden; */
+`;
+
 const App = () => {
+  let location = useLocation();
   return (
-    <Router>
-      <Switch>
-        <Route path='/' component={Home} exact />
-        <Route path='/attractions' component={AttractionsPage} exact />
-        <Route path='/offer' component={OfferPage} exact />
-        <Route path='/gallery' component={GalleryPage} exact />
-      </Switch>
-    </Router>
+    <Section>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route path='/' component={Home} exact />
+          <Route path='/attractions' component={AttractionsPage} />
+          <Route path='/offer' component={OfferPage} />
+          <Route path='/gallery' component={GalleryPage} />
+        </Switch>
+      </AnimatePresence>
+    </Section>
   );
 };
 
